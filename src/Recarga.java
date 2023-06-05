3099def89a5131b43a23007785a2af095b50e035
Lincoln Souza <lincolnf12@gmail.com>
2023-06-05 01:50:50 -0300

    Update Recarga.java

--- src/Recarga.java
+++ src/Recarga.java
@@ -22,6 +22,6 @@ public class Recarga {
     public String toString() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         String dataStr = dateFormat.format(data.getTime());
-        return "Data: " + dataStr + ", Valor: R$ " + valor;
+        return "Data: "+ dataStr +", Valor: R$" + valor;
     }
 }
