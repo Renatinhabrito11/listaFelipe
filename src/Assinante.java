8869512a09481a5dfe492cb95c4ea64cda948ed8
Renatinhabrito11 <114967604+Renatinhabrito11@users.noreply.github.com>
2023-06-04 23:58:35 -0300

    Atualizar o Assinante.java

--- src/Assinante.java
+++ src/Assinante.java
@@ -1,21 +1,24 @@
 public class Assinante {
-    private long cpf;
+//    private long cpf;
+    private int cpf
     private String nome;
     private String numero;
     private int numChamadas;
 
-    public Assinante(long cpf, String nome, String numero) {
+//    public Assinante(long cpf, String nome, String numero) {
+      public Assinante(int cpf, String nome, String numero) {
         this.cpf = cpf;
         this.nome = nome;
         this.numero = numero;
         this.setNumChamadas(0);
     }
 
-    public long getCpf() {
+//    public long getCpf() {
+      public int getCpf() {
         return cpf;
     }
 
-    @Override
+//    @Override
     public String toString() {
         return "CPF: " + cpf + "\nNome: " + nome + "\nNúmero: " + numero;
     }
